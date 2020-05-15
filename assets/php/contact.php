c577c9dd2b162b416bb5908eee97bfcf07ffc584
serhiiwolf <45642784+serhiiwolf@users.noreply.github.com>
2020-05-15 15:48:17 +0300

    change contact form php file

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -7,7 +7,7 @@ if(isset($_POST['message'])){
 	$message = $_POST['message'];
     
 	
-	$to      = 'you@example.com';
+	$to      = 'serhiird2@gmail.com';
 	$subject = 'Site Contact Form';
 
 	$headers = 'From: '. $email . "\r\n" .
