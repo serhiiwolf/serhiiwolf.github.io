0fc553c4ac71216b43c6df8dc6b6fe2f46990869
serhiiwolf <45642784+serhiiwolf@users.noreply.github.com>
2020-09-16 12:54:59 +0300

    time

---
+++ assets/js/st.js
@@ -0,0 +1,20 @@
+/*school time js*/
+
+var d = new Date();
+var h = d.getHours();
+var m = d.getMinutes();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var wd = weekday[d.getDay()]
+document.getElementById('std').innerHTML = wd;
+document.getElementById('sth').innerHTML = h;
+document.getElementById('stm').innerHTML = m;
+
+/* school */
