525549c219e5bb7d1caeaefef0ccca2be5d02694
Serhii <45642784+serhiiwolf@users.noreply.github.com>
2024-04-23 01:16:31 +0200

    new home flowers
    add mobile/tablet version; translate text

--- home flowers/js/script.js
+++ home flowers/js/script.js
@@ -30,4 +30,17 @@ function changePhoto(numb, pick) {
         
     }
     
+};
+changePhoto(5,0);
+
+function feedback(){
+    alert("thank you for your feedback");
 }
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
